1c6115ed1161b5cdce650c666c29a51991bd28ba
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-12-18 09:59:40 -0500

    🎨 [UPDATE] Feed Styles

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,10 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     '../templates/**/*.html'
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      pink: "#FFC4DD",
+      "dark-gray": "#222022",
+      dark: "#131313",
+      blue: "#2C13DD"
+    },
   },
   plugins: [],
 }
